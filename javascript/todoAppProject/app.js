fa01e388cd5889b73764d7ee66e5aa1422382b44
John Stuby <johnstuby7@gmail.com>
2024-02-14 20:08:06 -0700

    Todo Project: Updated app to have a ui controller that captures when the button is clicked and the text is inputed, add another controller to display a message if the text field is not empty. and finally, added in a event listener for when the enter key has been pressed

--- javascript/todoAppProject/app.js
+++ javascript/todoAppProject/app.js
@@ -1,3 +1,33 @@
-document.querySelector(".add_btn").addEventListener("click", function () {
-  console.log("Button was clicked");
+// Data Controller
+
+// UI Controller
+var DOMStrings = {
+  addBtn: document.querySelector(".add__btn"),
+  taskDescription: document.querySelector(".add__description"),
+};
+
+// App Controller
+// Will return the input data and the entered text to the console
+function ctrlAddTask() {
+  var input, text;
+
+  // Get input data from DOM
+  input = DOMStrings.taskDescription;
+  text = input.value;
+
+  // Check for Text
+  if (text) {
+    console.log("Input is not empty");
+    //  Add the task to the data structure
+    // Add the task to the UI
+    // Clear the field
+  }
+}
+
+DOMStrings.addBtn.addEventListener("click", ctrlAddTask);
+
+document.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13 || event.which === 13) {
+    ctrlAddTeask();
+  }
 });
