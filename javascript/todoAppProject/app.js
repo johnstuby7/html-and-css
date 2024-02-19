46e3fb7269c9a97c379b3c7bfc00bf86239a7c8f
John Stuby <johnstuby7@gmail.com>
2024-02-19 08:42:40 -0700

    Fixed issue with id, added in done button functionality, added in logic to the checkmark button to cross out item in too do list when clicked

--- javascript/todoAppProject/app.js
+++ javascript/todoAppProject/app.js
@@ -1,35 +1,37 @@
-// Data Controller
+// DATA CONTROLLER
 
 // Store tasks
 var taskStore = [];
 
-// Constructor for tasks
+// Constructor for task
 var Task = function (id, description) {
   this.id = id;
   this.description = description;
 };
 
-//  To test that the above works, in the console on the browser, add a new Task(0, 'test') and you should get a result
-
-// Add Task
+// Add task
 function addTask(des) {
-  var newTask;
-  ID = 0;
+  var newTask, ID;
 
-  // How to ensure that each id should be unique
+  //taskStore.length + 1;
+
+  //[1,2,3,4,5] id=6
+  //[1,2,5,6,8] id=9
+
+  // Create new ID
   if (taskStore.length > 0) {
     ID = taskStore[taskStore.length - 1].id + 1;
   } else {
     ID = 0;
   }
 
-  // Create new Task
+  // Create new task
   newTask = new Task(ID, des);
 
   // Push it into data structure
   taskStore.push(newTask);
 
-  // return the new task
+  // Return the new task
   return newTask;
 }
 
@@ -37,12 +39,16 @@ function addTask(des) {
 function deleteTask(id) {
   var ids, index;
 
-  // Create a array for ids
+  //taskStore[2]
+  //[1,2,5,6,8] id=5
+
+  // Create array for ids
   ids = taskStore.map(function (current) {
     return current.id;
   });
-  // Find IDs index
-  index = ids.index(parseInt(id));
+
+  // Find ids index
+  index = ids.indexOf(parseInt(id));
 
   // Delete task
   if (index !== -1) {
@@ -50,30 +56,59 @@ function deleteTask(id) {
   }
 }
 
-// UI Controller
+// UI CONTROLLER
+
+// Gather DOM strings same place
 var DOMStrings = {
   addBtn: document.querySelector(".add__btn"),
   taskDescription: document.querySelector(".add__description"),
   taskContainer: document.querySelector(".task__list"),
 };
 
+//Add task to UI
 function addListTask(task) {
   var html, newHtml, element;
-  // Create HTML strings with placeholder text
 
+  // Create HTML string with placeholder text
   html =
-    '<div class="item clearfix" id="%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__done"><button class="item__done--btn"><i class="ion-ios-checkmark-outline"></i></button></div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+    '<div class="item clearfix" id="%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__done"> <button class="item__done--btn"> <i class="ion-ios-checkmark-outline"></i> </button> </div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i> </button> </div> </div> </div>';
 
-  // Replace Placeholder with actual data
+  // Replace the placeholder text with some actual data
   newHtml = html.replace("%id%", task.id);
-  newHtml = html.replace("%description%", task.description);
-  // Insert the html into the DOM
+  newHtml = newHtml.replace("%description%", task.description);
+
+  // Insert the HTML into The DOM
   element = DOMStrings.taskContainer;
   element.insertAdjacentHTML("beforeend", newHtml);
 }
 
-// App Controller
-// Will return the input data and the entered text to the console
+// Delete task from UI
+function deleteListTask(selectorID) {
+  var el;
+
+  el = document.getElementById(selectorID);
+
+  // Remove HTML from DOM
+  el.remove();
+}
+
+// Done button
+function doneListTask(selectorID) {
+  var el;
+
+  el = document.getElementById(selectorID);
+
+  // Add new class for description
+  el.firstElementChild.classList.toggle("item__description__done");
+
+  // Add new class for done button
+  el.children[1].children[0].children[0].classList.toggle(
+    "item__done--btn--done"
+  );
+}
+
+// APP CONTROLLER
+
 function ctrlAddTask() {
   var input, text, newTask;
 
@@ -81,9 +116,9 @@ function ctrlAddTask() {
   input = DOMStrings.taskDescription;
   text = input.value;
 
-  // Check for Text
+  // Check for text
   if (text) {
-    //  Add the task to the data structure
+    // Add the task to the data structure
     newTask = addTask(text);
 
     // Add the task to the UI
@@ -96,16 +131,26 @@ function ctrlAddTask() {
 }
 
 function ctrlDeleteTask(event) {
-  var taskID;
+  var taskID, doneBtn, clickedElement;
 
-  // Find ID
+  doneBtn = "ion-ios-checkmark-outline";
+  clickedElement = event.target.className;
+
+  // Find id
   taskID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 
-  // Check if there is a ID
-  if (taskID) {
+  // Check done button
+  if (clickedElement === doneBtn) {
+    // Change UI
+    doneListTask(taskID);
+
+    // Check is there any ID
+  } else if (taskID) {
     // Delete the task from the data structure
     deleteTask(taskID);
+
     // Delete the task from the UI
+    deleteListTask(taskID);
   }
 }
 
@@ -113,7 +158,7 @@ DOMStrings.addBtn.addEventListener("click", ctrlAddTask);
 
 document.addEventListener("keypress", function (event) {
   if (event.keyCode === 13 || event.which === 13) {
-    ctrlAddTeask();
+    ctrlAddTask();
   }
 });
 
