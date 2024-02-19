53d5af33b793db6d8346707e7fa37df246fc4634
John Stuby <johnstuby7@gmail.com>
2024-02-19 07:54:50 -0700

    Added in a taskStore to store tasks, added a addTask function that ensures id will be unique, updated taskController to store the inputted tasks into the taskStore

--- javascript/todoAppProject/app.js
+++ javascript/todoAppProject/app.js
@@ -1,5 +1,38 @@
 // Data Controller
 
+// Store tasks
+var taskStore = [];
+
+// Constructor for tasks
+var Task = function (id, description) {
+  this.id = id;
+  this.description = description;
+};
+
+//  To test that the above works, in the console on the browser, add a new Task(0, 'test') and you should get a result
+
+// Add Task
+function addTask(des) {
+  var newTask;
+  ID = 0;
+
+  // How to ensure that each id should be unique
+  if (taskStore.length > 0) {
+    ID = taskStore[taskStore.length - 1].id + 1;
+  } else {
+    ID = 0;
+  }
+
+  // Create new Task
+  newTask = new Task(ID, des);
+
+  // Push it into data structure
+  taskStore.push(newTask);
+
+  // return the new task
+  return newTask;
+}
+
 // UI Controller
 var DOMStrings = {
   addBtn: document.querySelector(".add__btn"),
@@ -9,7 +42,7 @@ var DOMStrings = {
 // App Controller
 // Will return the input data and the entered text to the console
 function ctrlAddTask() {
-  var input, text;
+  var input, text, newTask;
 
   // Get input data from DOM
   input = DOMStrings.taskDescription;
@@ -17,8 +50,8 @@ function ctrlAddTask() {
 
   // Check for Text
   if (text) {
-    console.log("Input is not empty");
     //  Add the task to the data structure
+    newTask = addTask(text);
     // Add the task to the UI
     // Clear the field
   }
