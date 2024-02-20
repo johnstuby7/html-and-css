76fed6004fef503ac116668eca1df125d0e8728e
John Stuby <johnstuby7@gmail.com>
2024-02-19 18:06:01 -0700

    Add prop data to pass video data from parent to child

--- vue/video-portal/src/App.vue
+++ vue/video-portal/src/App.vue
@@ -2,9 +2,14 @@
   <div>
     <!-- When a newTerm is entered in the input, the handleNewTerm method is triggered -->
     <SearchInput @newTerm="handleNewTerm"></SearchInput>
-    <VideosDisplay></VideosDisplay>
+    <!--v-bind:videosDisplay this is the name of the property that we want to show up in the child, the right videosdisplay, is the value
+    that should be displayed in the parent(line 26 in components section)  
+    anytime videos is updated the child videosdisplay value will be updated-->
+    <VideosDisplay v-bind:videosDisplay="videosDisplay"></VideosDisplay>
+
+
     <!-- Below is the array of videos returned by search term -->
-    {{ videosDisplay.valueOf() }}
+    <!-- {{ videosDisplay.valueOf() }} -->
   </div>
 </template>
 
